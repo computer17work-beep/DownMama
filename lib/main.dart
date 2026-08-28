8405bcf49d71c0a4edeb733f90b2d249d0281f33
computer17work-beep <computer17work@gmail.com>
2026-08-28 15:59:12 +0600

    Create lib/main.dart

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'theme/app_theme.dart';
+
+void main() {
+  runApp(const DownMamaApp());
+}
+
+class DownMamaApp extends StatelessWidget {
+  const DownMamaApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'DownMama',
+      theme: AppTheme.darkTheme,
+      home: const Scaffold(
+        body: Center(
+          child: Text(
+            'DownMama',
+            style: TextStyle(
+              fontSize: 32,
+              fontWeight: FontWeight.bold,
+              color: AppColors.cyan,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
